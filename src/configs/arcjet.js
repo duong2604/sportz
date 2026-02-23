fcf8417d7cc2930efb09307ced04770d4523908f
duongbui <duongbuibd@gmail.com>
2026-02-23 17:49:38 +0700

    replace throwing an error if missing arcjet key by console warning

--- src/configs/arcjet.js
+++ src/configs/arcjet.js
@@ -1,9 +1,11 @@
 import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
 
 const arcjetKey = process.env.ARCJET_KEY;
-const arcjetMode = process.env.ARCJECT_MODE === "DRY_RUN" ? "DRY_RUN" : "LIVE";
+const arcjetMode = process.env.ARCJET_MODE === "DRY_RUN" ? "DRY_RUN" : "LIVE";
 
-if (!arcjetKey) throw new Error("ARCJET_KEY environment variable is missing.");
+if (!arcjetKey) {
+  console.warn("ARCJET_KEY not set – ArcJet protection disabled.");
+}
 
 export const httpArcjet = arcjetKey
   ? arcjet({
